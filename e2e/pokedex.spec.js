e58b2a2c578881118d1b631f76d9621a35f2d3fb
Amanda Uppgård <amanda.uppgard@abo.fi>
2026-03-04 20:43:32 +0200

    completed 11.9

--- e2e/pokedex.spec.js
+++ e2e/pokedex.spec.js
@@ -6,4 +6,10 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('page of a specific pokemon can be opened', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
